40753e2c73db459b341ad505adfa873a21ee66ac
Cody Jones <Cody Jones>
2017-11-06 03:05:18 -0800

    Updating calc controller

--- WebServer/controllers/calculationController.js
+++ WebServer/controllers/calculationController.js
@@ -1,10 +1,17 @@
 
+
+
 var calculateTaxes = function (req, res, next) {
 	console.log('found the function');
 	console.log(req.body);
 	var data = req.body;
 	console.log('income: ' + data.income);
 	console.log('status: ' + data.status);
+	
+	$.post("https://35.203.164.77/cutcutcut", postData, function(res, status){
+        console.log(res);
+        console.log(response);
+    }, "json");
 }
 
 
