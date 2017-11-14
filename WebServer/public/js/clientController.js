81de38022c73dc28fcb6afd858e28f3952749055
Cody Jones <Cody Jones>
2017-11-13 19:19:30 -0800

    Cleaned up code in clientController.js

--- WebServer/public/js/clientController.js
+++ WebServer/public/js/clientController.js
@@ -1,5 +1,7 @@
 
-$('#submit').click(function(){
+var restServerAddress = 'http://35.203.158.17/';
+
+$('#submit').click(function() {
 	calcTaxes();
 });
 
@@ -26,81 +28,82 @@ calcTaxes = function() {
 	console.log('   ' + JSON.stringify(postData));
 	console.log();
 
+	sendDataToRestServerAndUpdatePage(postData);
+
+}
+
+sendDataToRestServerAndUpdatePage = function(postData) {
 	$.ajax({
 		type: "POST",
-		url: "http://35.203.158.17/cutcutcut",
+		url: restServerAddress + 'cutcutcut',
 		data: JSON.stringify(postData),
-		success: function(res, status) {
-			try {
-				console.log('   Recieved successful response from Rest Server');
-				console.log('      The response is the following:');
-				console.log('         ' + JSON.stringify(res));
-				
-				var summaryMessage = document.getElementById('tax_results');
-				var percentSavingsMessage = document.getElementById('tax_results_percent');
-				var dollarSavings = Math.round(res.savingsUnderNewPlan * 100) / 100;
-				var inputIncome = document.getElementById('income').value;
-				var percentSavings = Math.abs(((dollarSavings / inputIncome) * 100).toFixed(2));
-				var current2018TaxSystem = res.Current2018System;
-				var currentEffectiveTaxRate = ((res.Current2018System.taxedAmountAfterCredits / inputIncome) * 100).toFixed(2);
-				var houseEffectiveTaxRate = ((res.TaxCutsAndJobsActHouse.taxedAmountAfterCredits / inputIncome) * 100).toFixed(2);
-
-				console.log('Type: ' + typeof(current2018TaxSystem));
-
-				document.getElementById('results').style.visibility = 'visible';
-				if (dollarSavings < 0)
-					dollarSavings *= -1;
-
-				if (res.savingsUnderNewPlan > 0) {
-					summaryMessage.innerHTML = "You will pay $" + dollarSavings.toFixed(2) + " less in taxes under the proposed house bill.";
-					summaryMessage.style.color = 'green';
-					percentSavingsMessage.innerHTML = "This is " + percentSavings + "% of your income";
-					percentSavingsMessage.style.color = 'green';
-				}
-				else {
-					summaryMessage.innerHTML = "You will pay $" + dollarSavings.toFixed(2) + " more in taxes under the proposed house bill.";
-					summaryMessage.style.color = 'red';
-					percentSavingsMessage.innerHTML = "This is " + percentSavings + "% of your income";
-					percentSavingsMessage.style.color = 'red';
-				}
-
-				console.log("         This will be the data sent to the server at resultsView/current2018TaxSystem:");
-				console.log('            ' + JSON.stringify(current2018TaxSystem));
-
-				$.ajax({
-					type: 'POST',
-					url: '/resultsView/current2018TaxSystem',
-					data: JSON.stringify(current2018TaxSystem),
-					contentType: 'application/json',
-					success: function(res, status) {
-						console.log('         Recieved a succesful response to /resultsView/current2018TaxSystem');
-						console.log('         The response is the following:');
-						console.log('            ' + res);
-						var resultsDiv = document.getElementById('result-details-current2018System');
-						resultsDiv.innerHTML = res;
-					},
-					error: function(jqXHR, textStatus, errorThrown) {
-						console.log("         Error thrown in post to resultsView/current2018TaxSystem in clientController.js, status = " + textStatus);
-						console.log("            Error: " + errorThrown);
-						/*
-						alert("Error thrown in post to resultsView/current2018TaxSystem in clientController.js, status = " + textStatus + ", " +
-							"error thrown: " + errorThrown);
-						*/
-					}
-				});
-
-			}
-			catch(exception) {
-				console.log('Caught Exception: ' + exception.message);
-			}
-    	},
+		success: updatePageWithRestServerResponse,
 		error: function(jqXHR, textStatus, errorThrown) {
 			console.log("   Error thrown in post to Rest Server /cutcutcut in clientController.js, status = " + textStatus);
 			console.log("      Error: " + errorThrown);
-			/*
-			alert("Error, status = " + textStatus + ", " +
-				"error thrown: " + errorThrown);
-			*/
 		}
 	});
+}
+
+updatePageWithRestServerResponse = function(res, status) {
+	try {
+		console.log('   Recieved successful response from Rest Server');
+		console.log('      The response is the following:');
+		console.log('         ' + JSON.stringify(res));
+		
+		var summaryMessage = document.getElementById('tax_results');
+		var percentSavingsMessage = document.getElementById('tax_results_percent');
+		var dollarSavings = Math.round(res.savingsUnderNewPlan * 100) / 100;
+		var inputIncome = document.getElementById('income').value;
+		var percentSavings = Math.abs(((dollarSavings / inputIncome) * 100).toFixed(2));
+		var current2018TaxSystem = res.Current2018System;
+		var currentEffectiveTaxRate = ((res.Current2018System.taxedAmountAfterCredits / inputIncome) * 100).toFixed(2);
+		var houseEffectiveTaxRate = ((res.TaxCutsAndJobsActHouse.taxedAmountAfterCredits / inputIncome) * 100).toFixed(2);
+
+		console.log('Type: ' + typeof(current2018TaxSystem));
+
+		document.getElementById('results').style.visibility = 'visible';
+		if (dollarSavings < 0)
+			dollarSavings *= -1;
+
+		if (res.savingsUnderNewPlan > 0) {
+			summaryMessage.innerHTML = "You will pay $" + dollarSavings.toFixed(2) + " less in taxes under the proposed house bill.";
+			summaryMessage.style.color = 'green';
+			percentSavingsMessage.innerHTML = "This is " + percentSavings + "% of your income";
+			percentSavingsMessage.style.color = 'green';
+		}
+		else {
+			summaryMessage.innerHTML = "You will pay $" + dollarSavings.toFixed(2) + " more in taxes under the proposed house bill.";
+			summaryMessage.style.color = 'red';
+			percentSavingsMessage.innerHTML = "This is " + percentSavings + "% of your income";
+			percentSavingsMessage.style.color = 'red';
+		}
+
+		console.log("         This will be the data sent to the server at resultsView/current2018TaxSystem:");
+		console.log('            ' + JSON.stringify(current2018TaxSystem));
+
+		$.ajax({
+			type: 'POST',
+			url: '/resultsView/current2018TaxSystem',
+			data: JSON.stringify(current2018TaxSystem),
+			contentType: 'application/json',
+			success: updateResultsSubViewFor2018System,
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("         Error thrown in post to resultsView/current2018TaxSystem in clientController.js, status = " + textStatus);
+				console.log("            Error: " + errorThrown);
+			}
+		});
+	}
+	catch(exception) {
+		console.log('Caught Exception: ' + exception.message);
+	}
+
+}
+
+updateResultsSubViewFor2018System = function(res, status) {
+	console.log('         Recieved a succesful response to /resultsView/current2018TaxSystem');
+	console.log('         The response is the following:');
+	console.log('            ' + res);
+	var resultsDiv = document.getElementById('result-details-current2018System');
+	resultsDiv.innerHTML = res;
 }
